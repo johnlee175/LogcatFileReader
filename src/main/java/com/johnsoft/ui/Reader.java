b6786e634e37ad5216f60b9f9fe60960064f2caf
lizhehao <lizhehao@baidu.com>
2016-05-10 08:48:02 +0800

    add log file and format chooser

--- src/main/java/com/johnsoft/ui/Reader.java
+++ src/main/java/com/johnsoft/ui/Reader.java
@@ -17,18 +17,30 @@
 package com.johnsoft.ui;
 
 import java.awt.BorderLayout;
+import java.awt.Component;
 import java.awt.EventQueue;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
 import java.io.File;
 import java.io.IOException;
 import java.util.List;
 
+import javax.swing.JComboBox;
+import javax.swing.JDialog;
+import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JList;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import javax.swing.ListCellRenderer;
 import javax.swing.UIManager;
 import javax.swing.WindowConstants;
+import javax.swing.filechooser.FileFilter;
 
 import com.johnsoft.logcat.LogCatMessage;
 import com.johnsoft.logcat.LogCatMessageParser;
@@ -123,36 +135,139 @@ public class Reader {
         EventQueue.invokeLater(new Runnable() {
             @Override
             public void run() {
+                show();
+            }
+        });
+    }
+
+    private static File logFile;
+    private static LogCatMessageParser parser;
+
+    public static void main(final String[] args) {
+        EventQueue.invokeLater(new Runnable() {
+            @Override
+            public void run() {
                 try {
                     UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
                 } catch (Exception e) {
                     JOptionPane.showMessageDialog(null, "No optional system style UI skin");
                     e.printStackTrace();
                 }
-                show();
+
+                parser = new LogCatMessageParser(); //default: -v long
+                if (args.length > 0) {
+                    logFile = new File(args[0]);
+                    if (!logFile.exists() || !logFile.isFile() || !logFile.canRead()) {
+                        logFile = null;
+                    }
+                    if (args.length == 2) {
+                        switch (args[1]) {
+                            case "threadtime":
+                                parser = new LogCatMessageParser2();
+                                break;
+                            case "javacustom":
+                                parser = new LogCatMessageParser3();
+                                break;
+                        }
+                    }
+                } else {
+                    final JDialog dialog = new JDialog((JFrame)null, true);
+                    final JFileChooser fileChooser = new JFileChooser();
+                    fileChooser.setDragEnabled(true);
+                    fileChooser.setFileHidingEnabled(false);
+                    fileChooser.setMultiSelectionEnabled(false);
+                    fileChooser.setFileSelectionMode(JFileChooser.FILES_ONLY);
+                    fileChooser.setFileFilter(new FileFilter() {
+                        @Override
+                        public boolean accept(File f) {
+                            return f.getName().endsWith(".log");
+                        }
+                        @Override
+                        public String getDescription() {
+                            return "*.log";
+                        }
+                    });
+                    fileChooser.addActionListener(new ActionListener() {
+                        @Override
+                        public void actionPerformed(ActionEvent e) {
+                            if (JFileChooser.APPROVE_SELECTION.equals(e.getActionCommand())) {
+                                logFile = fileChooser.getSelectedFile();
+                            }
+                            dialog.dispose();
+                        }
+                    });
+                    final JComboBox<String> formatExample = new JComboBox<>(new String[]{
+                            "[ 04-25 21:53:28.440  1190: 1190 I/MyTag ]\n"
+                                    + "this is message body",
+                            "04-25 21:53:28.440  1190  1190 I MyTag: this is message body",
+                            "[2016-04-25 21:53:28.440][main][INFO ]MyTag - this is message body"
+                    });
+                    formatExample.setRenderer(new ListCellRenderer<String>() {
+                        private JTextArea item = new JTextArea();
+                        @Override
+                        public Component getListCellRendererComponent(JList<? extends String> list, String value,
+                                                         int index, boolean isSelected, boolean cellHasFocus) {
+                            item.setText(value);
+                            if (isSelected) {
+                                item.setBackground(list.getSelectionBackground());
+                                item.setForeground(list.getSelectionForeground());
+                            } else {
+                                item.setBackground(list.getBackground());
+                                item.setForeground(list.getForeground());
+                            }
+                            return item;
+                        }
+                    });
+                    formatExample.setEditable(false);
+                    final String labelPrefix = "log file text format:";
+                    final JLabel formatDescription = new JLabel(labelPrefix + " long ");
+                    formatExample.addItemListener(new ItemListener() {
+                        @Override
+                        public void itemStateChanged(ItemEvent e) {
+                            if (e.getStateChange() == ItemEvent.SELECTED) {
+                                switch (formatExample.getSelectedIndex()) {
+                                    case 0:
+                                        formatDescription.setText(labelPrefix + " long ");
+                                        break;
+                                    case 1:
+                                        formatDescription.setText(labelPrefix + " threadtime ");
+                                        break;
+                                    case 2:
+                                        formatDescription.setText(labelPrefix + " javacustom ");
+                                        break;
+                                    default:
+                                        formatDescription.setText(labelPrefix + " long ");
+                                        break;
+                                }
+                            }
+                        }
+                    });
+                    JPanel bottom = new JPanel(new BorderLayout());
+                    bottom.add(formatDescription, BorderLayout.WEST);
+                    bottom.add(formatExample, BorderLayout.CENTER);
+                    JPanel panel = new JPanel(new BorderLayout());
+                    panel.add(new JLabel("which log text file would you want to open: "), BorderLayout.NORTH);
+                    panel.add(fileChooser, BorderLayout.CENTER);
+                    panel.add(bottom, BorderLayout.SOUTH);
+                    dialog.setContentPane(panel);
+                    dialog.setTitle("Open Dialog");
+                    dialog.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
+                    dialog.setAlwaysOnTop(true);
+                    dialog.setSize(panel.getPreferredSize());
+                    dialog.setLocationRelativeTo(null);
+                    dialog.setVisible(true);
+                    switch (formatExample.getSelectedIndex()) {
+                        case 1:
+                            parser = new LogCatMessageParser2();
+                            break;
+                        case 2:
+                            parser = new LogCatMessageParser3();
+                            break;
+                    }
+                }
+                new Reader().main(logFile, parser);
             }
         });
     }
 
-    public static void main(String[] args) {
-        File logFile = null;
-        LogCatMessageParser parser = new LogCatMessageParser(); //default: -v long
-        if (args.length > 0) {
-            logFile = new File(args[0]);
-            if (!logFile.exists() || !logFile.isFile() || !logFile.canRead()) {
-                logFile = null;
-            }
-            if (args.length == 2) {
-                switch (args[1]) {
-                    case "threadtime":
-                        parser = new LogCatMessageParser2();
-                        break;
-                    case "javacustom":
-                        parser = new LogCatMessageParser3();
-                        break;
-                }
-            }
-        }
-        new Reader().main(logFile, parser);
-    }
 }
